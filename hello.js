91e7703b9813e85e51da62eb60b1f3b83d3601f1
myname <my@gmail.com>
2023-11-08 11:01:48 +0530

    forbidden directory added

--- hello.js
+++ hello.js
@@ -3,4 +3,9 @@ console.log('Hello world')
 console.log('edited')
 console.log('added a new line')
 console.log('rebase')
-console.log('add another line')
+console.log('add another line'
+c
+)
+console.log('hook')
+
+
