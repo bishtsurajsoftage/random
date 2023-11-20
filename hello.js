fa221667f206b8250ac2c85872227698760aaa57
Suraj <suraj@gmail.com>
2023-11-20 11:27:14 +0530

    d:

--- hello.js
+++ hello.js
@@ -3,9 +3,14 @@ console.log('Hello world')
 console.log('edited')
 console.log('added a new line')
 console.log('rebase')
+<<<<<<< HEAD
 console.log('add another line'
 c
 )
 console.log('hook')
 
 
+=======
+console.log('add another line')
+console.log('added')
+>>>>>>> parent of 7a96aea (Update hello.js)
