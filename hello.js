1622798a751d574263caf0a5bda78c5055554ffb
softageb <surajb.softage@gmail.com>
2023-11-23 16:12:33 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,14 +3,12 @@ console.log('Hello world')
 console.log('edited')
 console.log('added a new line')
 console.log('rebase')
-<<<<<<< HEAD
+
+
 console.log('add another line'
 c
 )
 console.log('hook')
-
-
-=======
 console.log('add another line')
 console.log('added')
 
